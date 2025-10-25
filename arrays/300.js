4f6d8d46b3ad872e1a0ced886cf903b4231b55e0
kohanman <kohanman@googlemail.com>
2025-10-25 11:10:30 +0100

    did the arrays workshops

--- arrays/300.js
+++ arrays/300.js
@@ -1,16 +1,42 @@
 // the head of an array is the first element of an array
 // the tail of an array is the last element of an array
 
-
-const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 function getHead(arr) {
-    return arr.shift();   
-};
+  return arr.slice(0, 1);
+}
 
 function getTail(arr) {
-    return arr.pop();
-};
+  return arr.slice(-1);
+}
 
 console.log(`The first letter of the alphabet is ${getHead(alphabet)}`);
 console.log(`The last letter of the alphabet is ${getTail(alphabet)}`);
